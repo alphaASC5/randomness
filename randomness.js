4c27947bb56f64d57db38586714ea7380e9e441f
alphaASC5 <barriea@bxscience.edu>
2018-07-12 10:44:19 -0300

    I finished the project.

---
+++ randomness.js
@@ -0,0 +1,58 @@
+// 1. function randomNumber(){
+//     const decimal = Math.random();
+//     const times20 = decimal * 20;
+//     const random = Math.floor(times20);
+//     console.log(random);
+// }
+
+// randomNumber();
+
+//2. const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// function randomLetter(){
+//     const decimal = Math.random();
+//     const times26 = decimal * 26
+//     const num = Math.floor(times26);
+//     console.log(alphabet[num]);
+// }
+
+// randomLetter();
+
+//3. const food = ['pizza', 'chicken', 'rice', 'chips', 'cake', 'pie'];
+
+// function randomFood(){
+//     const decimal = Math.random();
+//     const times6 = decimal * 6;
+//     const num = Math.floor(times6);
+//     console.log(food[num]);
+// }
+
+// randomFood();
+
+//4. const students = ['Jeff', 'Blake', 'Shameek', 'Demari', 'Andrew'];
+
+// function randomStudents(){
+//     const decimal = Math.random();
+//     const times5 = decimal * 5;
+//     const num = Math.floor(times5);
+//     console.log(students[num]);
+// }
+
+// randomStudents();
+
+// const students = ['Jeff', 'Blake', 'Shameek', 'Demari', 'Andrew'];
+
+// function randomStudents(){
+//     const decimal = Math.random();
+//     const times5 = decimal * 5;
+//     const num = Math.floor(times5);
+//     console.log(students[num]);
+// }
+
+// function randomGroup(){
+//     randomStudents();
+//     randomStudents();
+//     randomStudents();
+// }
+
+// randomGroup();
